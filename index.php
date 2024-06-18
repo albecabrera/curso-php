7dcf4a1aecccc1e161bd75b2f9d7936ba4df4f8f
albecabrera <albe.cabrera@yahoo.de>
2024-06-18 21:24:39 +0200

    commit -m "change2"

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
-<html>
- <head>
-  <title>Prueba de PHP</title>
- </head>
- <body>
- <?php echo '<p>Hola Mundo</p>'; ?>
- </body>
-</html>
+
+<div class="form"><form action="accion.php" method="post">
+ <p>Su nombre: <input type="text" name="nombre" /></p>
+ <p>Su edad: <input type="text" name="edad" /></p>
+ <p><input type="submit" /></p>
+</form>
+</div>
+
+
+<style>
+.form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+  border: black 1px solid;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+}
+</style>
