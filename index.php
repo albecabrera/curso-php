863aff99c5eb7a6e7a04267a2e1d7302101b07c0
albecabrera <albe.cabrera@yahoo.de>
2024-06-22 11:23:23 +0200

    commit -m "change 4"

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
+<?php
+echo "Hola mundo";
+?>
 
-<div class="form"><form action="accion.php" method="post">
- <p>Su nombre: <input type="text" name="nombre" /></p>
- <p>Su edad: <input type="text" name="edad" /></p>
- <p><input type="submit" /></p>
-</form>
-</div>
-
-
-<style>
-.form {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 50px;
-  border: black 1px solid;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
-  text-align: center;
-}
-</style>
